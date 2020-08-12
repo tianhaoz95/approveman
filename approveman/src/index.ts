16e1448795cd11053d9d8e8aa830e4eaa026b931
Tianhao Zhou <tianhaoz@umich.edu>
2020-08-12 22:00:30 +0000

    chore: change gitpod starting tasks

--- approveman/src/index.ts
+++ approveman/src/index.ts
@@ -1,7 +1,7 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
+  app.on('pull_requests.opened', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
   })
