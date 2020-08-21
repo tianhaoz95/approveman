1fd0c8c394e934cd5300ad0b3f65dcc25b8dd984
Tianhao Zhou <tianhaoz@umich.edu>
2020-08-21 07:29:11 +0000

    dev: add first test

--- approveman/test/index.test.ts
+++ approveman/test/index.test.ts
@@ -1,17 +1,24 @@
-// You can import your modules
-// import index from '../src/index'
+// For more information about testing with Jest see:
+// https://facebook.github.io/jest/
+
+// For more information about using TypeScript in your tests, Jest recommends:
+// https://github.com/kulshekhar/ts-jest
+
+// For more information about testing with Nock see:
+// https://github.com/nock/nock
 
 import nock from 'nock'
-// Requiring our app implementation
-import myProbotApp from '../src'
+// Requiring the app implementation
+import approvemanApp from '../src'
 import { Probot } from 'probot'
-// Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+// Requiring the fixtures
+import prReopenedPayload from './fixtures/pr.reopened.json'
 const fs = require('fs')
 const path = require('path')
 
-describe('My Probot app', () => {
+jest.setTimeout(30000)
+
+describe('Approveman tests', () => {
   let probot: any
   let mockCert: string
 
@@ -26,26 +33,38 @@ describe('My Probot app', () => {
   beforeEach(() => {
     nock.disableNetConnect()
     probot = new Probot({ id: 123, cert: mockCert })
-    // Load our app into probot
-    probot.load(myProbotApp)
+    probot.load(approvemanApp)
   })
 
-  test('creates a comment when an issue is opened', async (done) => {
-    // Test that we correctly return a test token
+  test('approve when file matches ownership rules', async (done) => {
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
       .reply(200, { token: 'test' })
 
-    // Test that a comment is posted
     nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
+      .get('/repos/tianhaoz95/approveman-test/contents/.github/approveman.yml')
+      .reply(404)
+
+    nock('https://api.github.com')
+      .get('/repos/tianhaoz95/.github/contents/.github/approveman.yml')
+      .reply(404)
+
+    nock('https://api.github.com')
+      .post('/repos/tianhaoz95/approveman-test/pulls/1/reviews', (body: any) => {
+        done(expect(body).toMatchObject({
+          event: 'APPROVE'
+        }))
         return true
       })
       .reply(200)
 
-    // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    nock('https://api.github.com')
+      .get('/repos/tianhaoz95/approveman-test/pulls/1/files')
+      .reply(200, [
+        { filename: 'experimental/tianhaoz95/test.md' }
+      ])
+
+    await probot.receive({ name: 'pull_request', payload: prReopenedPayload })
   })
 
   afterEach(() => {
@@ -53,12 +72,3 @@ describe('My Probot app', () => {
     nock.enableNetConnect()
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
