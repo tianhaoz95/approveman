51c6fc1e7eedf48220e8ca8728e1d104e993d734
Tianhao Zhou <tianhaoz@umich.edu>
2020-08-29 16:21:49 -0700

    fix: add missing colon

--- test/fixtures/files/index.ts
+++ test/fixtures/files/index.ts
@@ -14,7 +14,7 @@ export const GITHUB_CONFIG_FILES = [
     ".github/config_0.yml",
     ".github/config_1.yml",
     ".github/config_2.yml",
-]
+];
 
 /**
  * Mocks a list of GitHub workflow related files.
@@ -23,7 +23,7 @@ export const GITHUB_WORKFLOW_FILE = [
     ".github/workflows/test_0.yml",
     ".github/workflows/test_1.yml",
     ".github/workflows/test_2.yml",
-]
+];
 
 /**
  * Mocks the prefix of a submodule location.
